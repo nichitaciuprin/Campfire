934effc3e6b1388745d2090995cf84c4170933ac
Nichita Ciuprin <nichitaciuprin@gmail.com>
2024-11-02 12:58:55 +0200

    UPDATE

--- src/SoundTest/build.bat
+++ src/SoundTest/build.bat
@@ -1,10 +1,8 @@
 @echo off
 setlocal
 
-set root=%~dp0..\..
-set deps=%root%\deps
-set batdir=%~dp0
-set build=%batdir%build
+set deps=%~dp0..\..\deps
+set build=%~dp0build
 
 @REM -O      Same as -O1
 @REM -O0     do no optimize, the default if no optimization level is specified
